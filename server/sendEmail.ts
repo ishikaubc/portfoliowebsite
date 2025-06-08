5295b98173739f4886d5cd42a8aa51cd6bb0a397
Ishika Agarwal <ishikaubc@gmail.com>
2025-06-08 14:26:10 +0545

    send email changes

--- server/sendEmail.ts
+++ server/sendEmail.ts
@@ -2,69 +2,64 @@
 
 import { Resend } from "resend";
 
-const validateString = (
-    value: unknown,
-    maxLength: number
-): value is string => {
-    if (!value || typeof value !== "string" || value.length > maxLength) {
-        return false;
-    }
+const validateString = (value: unknown, maxLength: number): value is string => {
+  if (!value || typeof value !== "string" || value.length > maxLength) {
+    return false;
+  }
 
-    return true;
+  return true;
 };
 
 const getErrorMessage = (error: unknown): string => {
-    let message: string;
+  let message: string;
 
-    if (error instanceof Error) {
-        message = error.message;
-    } else if (error && typeof error === "object" && "message" in error) {
-        message = String(error.message);
-    } else if (typeof error === "string") {
-        message = error;
-    } else {
-        message = "Something went wrong";
-    }
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message);
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Something went wrong";
+  }
 
-    return message;
+  return message;
 };
-
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-    const senderEmail = formData.get("senderEmail")
-    const message = formData.get("message")
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
 
-
-    // simple server-side validation
-    if (!validateString(senderEmail, 500)) {
-        return {
-            error: "Invalid sender email",
-        };
-    }
-    if (!validateString(message, 5000)) {
-        return {
-            error: "Invalid message",
-        };
-    }
-
-    let data;
-    try {
-        data = await resend.emails.send({
-            from: 'Contact Form <onboarding@resend.dev>',
-            to: 'patelhparth@gmail.com',
-            subject: "Message from contact form: Portfolio",
-            reply_to: senderEmail,
-            text: message as string
-        });
-    } catch (error: unknown) {
-        return {
-            error: getErrorMessage(error),
-        };
-    }
-
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
     return {
-        data,
+      error: "Invalid sender email",
     };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: "Ishika Agarwal <contact@ishikaubc.dev>",
+      to: "ishika03@student.ubc.ca",
+      subject: "Message from contact form: Portfolio",
+      reply_to: senderEmail,
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
+
+  return {
+    data,
+  };
 };
